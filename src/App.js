a5e1d33e6ba7ad38041f4a51218b7efd015cf759
Светлана Юхимович <svetik.iukhymovych@gmail.com>
2022-08-10 17:30:27 +0300

    react

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import './App.css';
+/*Завдання 1*/
+/*function App() {
+  return (
+    <>
+      <h1>Hello React</h1>
+      </>
+  );
+}*/
+/*Завдання 3*/ 
+class App extends Component{
+  render(){
+    return <div>
+      <img src="/img/logo.svg" alt="react" title="react"/>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </div>;
+  }
+}
+
+export default App;
